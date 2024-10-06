a295ddd47f67e48069a3a9d48375c9aeccc09aea
Dimitrov Ivan <mmo_unstable@abv.bg>
2024-10-06 18:41:07 +0300

    -add JSON to DTO mapper, with contract imposed by InternalValidator interface.

---
+++ src/main/java/com/dimitrov_solutions/weight_tracker/models/weather/InternalValidator.java
@@ -0,0 +1,12 @@
+package com.dimitrov_solutions.weight_tracker.models.weather;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public interface InternalValidator {
+    String validateToString(JsonNode node,String fieldName);
+
+    Double validateToDouble(JsonNode node, String fieldName);
+
+    Integer validateToInt(JsonNode node, String fieldName);
+
+}
