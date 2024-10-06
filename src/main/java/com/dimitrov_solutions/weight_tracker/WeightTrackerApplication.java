9c357f880599bf06f8e80fc826a33eb66bdd9dba
Dimitrov Ivan <mmo_unstable@abv.bg>
2024-10-06 18:29:11 +0300

    -add Entry point for application

---
+++ src/main/java/com/dimitrov_solutions/weight_tracker/WeightTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dimitrov_solutions.weight_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeightTrackerApplication.class, args);
+    }
+
+}
