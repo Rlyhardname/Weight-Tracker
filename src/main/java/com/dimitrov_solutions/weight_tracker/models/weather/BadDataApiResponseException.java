6230c8e053b78e3d77b0da7465677260a1b2f82a
Dimitrov Ivan <mmo_unstable@abv.bg>
2024-10-06 18:41:55 +0300

    -add Enum for feather states -add CustomException for global exception handling

---
+++ src/main/java/com/dimitrov_solutions/weight_tracker/models/weather/BadDataApiResponseException.java
@@ -0,0 +1,7 @@
+package com.dimitrov_solutions.weight_tracker.models.weather;
+
+// TODO Global
+public class BadDataApiResponseException extends RuntimeException {
+    public BadDataApiResponseException() {
+    }
+}
