1b7b51b6201884b11f8cc00a8a1a65b9bd2104bd
Dimitrov Ivan <mmo_unstable@abv.bg>
2024-10-19 17:05:29 +0300

    -add Service for building requests based on url and sending to weather API. - Send() Returns response body as JSON String that needs to be mapped to DetailsDto by DetailsDtoMapper

---
+++ src/main/java/com/dimitrov_solutions/weight_tracker/models/weather/beans/HttpClientFacadeService.java
@@ -0,0 +1,41 @@
+package com.dimitrov_solutions.weight_tracker.models.weather.beans;
+
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+@Service
+public class HttpClientFacadeService {
+    private final HttpClient client;
+
+    public HttpClientFacadeService() {
+        client = HttpClient.newHttpClient();
+    }
+
+    /**
+     *
+     * @param url
+     * @return ResponseBody from API request.
+     */
+    public String send(String url) {
+        Callable<HttpResponse<String>> response = () -> client.send(buildRequest(url), HttpResponse.BodyHandlers.ofString());
+        try {
+            return response.call().body();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private HttpRequest buildRequest(String url) {
+        return HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("accept", "text/html")
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+    }
+}
