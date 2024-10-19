e8925fefe86a5926765dbf7abf68021895c7c6b2
Dimitrov Ivan <mmo_unstable@abv.bg>
2024-10-19 17:08:40 +0300

    -add 2 beans and Enum for validation on incoming data about country,city and for validating inputs of weather api for DetailsDto creation mainly for choosing image name.

---
+++ src/main/java/com/dimitrov_solutions/weight_tracker/models/weather/beans/State.java
@@ -0,0 +1,23 @@
+package com.dimitrov_solutions.weight_tracker.models.weather.beans;
+
+public enum State {
+    CLEAR_SKY("clear sky"),
+    FEW_CLOUDS("few clouds"),
+    SCATTERED_CLOUDS("scattered clouds"),
+    BROKEN_CLOUDS("broken_clouds"),
+    RAIN("rain"),
+    SHOWER_RAIN("shower rain"),
+    THUNDER_STORM("thunder storm"),
+    SNOW("snow"),
+    MIST("mist");
+
+    private final String weather;
+
+    public String getWeather() {
+        return weather;
+    }
+
+    State(String weather) {
+        this.weather = weather;
+    }
+}
---
+++ src/main/java/com/dimitrov_solutions/weight_tracker/models/weather/beans/StateMap.java
@@ -0,0 +1,29 @@
+package com.dimitrov_solutions.weight_tracker.models.weather.beans;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class StateMap {
+    private final Map<String, String> weatherStateMap = new HashMap<>(Map.of(
+            "01d", "CLEAR_SKY",
+            "02d", "FEW_CLOUDS",
+            "03d", "SCATTERED_CLOUDS",
+            "04d", "BROKEN_CLOUDS",
+            "09d", "RAIN",
+            "10d", "SHOWER_RAIN",
+            "11d", "SHOWER_RAIN",
+            "13d", "SNOW",
+            "50d", "MIST"
+    ));
+
+    public boolean contains(String state) {
+        return weatherStateMap.containsKey(state);
+    }
+
+    public String getEnumValue(String state) {
+        return weatherStateMap.get(state);
+    }
+}
