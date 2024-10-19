216a51ddb7a571c33564923ef3997402064d3a32
Dimitrov Ivan <mmo_unstable@abv.bg>
2024-10-19 16:17:38 +0300

    -refactor

--- src/main/java/com/dimitrov_solutions/weight_tracker/WeightTrackerApplication.java
+++ src/main/java/com/dimitrov_solutions/weight_tracker/WeightTrackerApplication.java
@@ -1,8 +1,5 @@
 package com.dimitrov_solutions.weight_tracker;
 
-import com.dimitrov_solutions.weight_tracker.enums.CountryCodeCapital;
-import com.dimitrov_solutions.weight_tracker.enums.CountryCountryCode;
-import com.dimitrov_solutions.weight_tracker.models.weather.utils.UrlFacade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,13 +8,6 @@ public class WeightTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WeightTrackerApplication.class, args);
-        String url = UrlFacade.createUrl(CountryCountryCode.Cuba.getCode(), CountryCodeCapital.CU.getCapitalCity());
-        System.out.println(url);
-//        WeatherHttpClientFacade facade = new WeatherHttpClientFacade(url);
-//        WeatherMapFacade mapper = new WeatherMapFacade(new WeatherDetailsDtoMapper());
-//        String body = facade.send();
-//        mapper.mapTo(body, url);
-
     }
 
 }
