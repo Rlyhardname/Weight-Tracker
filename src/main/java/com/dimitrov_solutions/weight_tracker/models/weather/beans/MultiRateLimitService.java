b11e7414a3b447539ee95a01eb97445f6b097174
Dimitrov Ivan <mmo_unstable@abv.bg>
2024-10-20 12:49:16 +0300

    -add RateLimit service for WeatherApi endpoint

---
+++ src/main/java/com/dimitrov_solutions/weight_tracker/models/weather/beans/MultiRateLimitService.java
@@ -0,0 +1,27 @@
+package com.dimitrov_solutions.weight_tracker.models.weather.beans;
+
+import io.github.bucket4j.Bucket;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class MultiRateLimitService {
+
+    private final Map<String, Bucket> cache = new ConcurrentHashMap<>();
+
+    public Bucket resolveBucket(String email) {
+        return cache.computeIfAbsent(email, this::newBucket);
+    }
+
+    private Bucket newBucket(String email) {
+        return Bucket.builder()
+                .addLimit((limit) ->
+                        limit.capacity(1)
+                                .refillIntervally(1, Duration.ofHours(1))).build();
+
+    }
+
+}
