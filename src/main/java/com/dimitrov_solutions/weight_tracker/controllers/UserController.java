c91000631861318f958462a2c663b44b4549edfb
Dimitrov Ivan <mmo_unstable@abv.bg>
2024-10-20 16:53:24 +0300

    -add UserController with endpoints /new for registering new users and /token for verification and receiving token from server

---
+++ src/main/java/com/dimitrov_solutions/weight_tracker/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.dimitrov_solutions.weight_tracker.controllers;
+
+import com.dimitrov_solutions.weight_tracker.exceptions.InvalidCredentialsException;
+import com.dimitrov_solutions.weight_tracker.models.dto.UserDto;
+import com.dimitrov_solutions.weight_tracker.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/users")
+@RestController
+@Validated
+public class UserController {
+    private static final String BAD_CREDENTIALS = "Email or password is invalid";
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<UserDto> register(@RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.register(userDto));
+    }
+
+    @PostMapping("/token")
+    public ResponseEntity<String> login(@RequestParam String email, @RequestParam String password) {
+        try {
+            return ResponseEntity.ok(userService.verify(email, password));
+        } catch (InvalidCredentialsException e) {
+            return ResponseEntity.status(401).body(BAD_CREDENTIALS);
+        }
+    }
+}
