d2b19614b5cb8bdf741b47097e8f0a5ca4c34760
Dimitrov Ivan <mmo_unstable@abv.bg>
2024-10-20 13:55:04 +0300

    -add validation for successful response

--- src/main/java/com/dimitrov_solutions/weight_tracker/weather/network/HttpClientFacadeService.java
+++ src/main/java/com/dimitrov_solutions/weight_tracker/weather/network/HttpClientFacadeService.java
@@ -1,15 +1,18 @@
 package com.dimitrov_solutions.weight_tracker.weather.network;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.concurrent.Callable;
 
 @Service
 public class HttpClientFacadeService {
+    private static final Logger logger = LoggerFactory.getLogger(HttpClientFacadeService.class);
     private final HttpClient client;
 
     public HttpClientFacadeService() {
@@ -17,17 +20,23 @@ public class HttpClientFacadeService {
     }
 
     /**
-     *
      * @param url
      * @return ResponseBody from API request.
      */
     public String send(String url) {
-        Callable<HttpResponse<String>> response = () -> client.send(buildRequest(url), HttpResponse.BodyHandlers.ofString());
         try {
-            return response.call().body();
-        } catch (Exception e) {
+            HttpResponse<String> response = client.send(buildRequest(url), HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() != 200) {
+                logger.debug("Api failed response!");
+                throw new RuntimeException();
+            }
+
+            return response.body();
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
+
     }
 
     private HttpRequest buildRequest(String url) {
